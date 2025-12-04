5bd0dbd312807c2ce8402a0ccdcccabec4596bba
Tharun-Kumar-A-01 <tharunkumar8906@gmail.com>
2025-12-04 16:38:09 +0530

    finished about section

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("scroll",(e)=>{
     if(this.oldScroll > this.scrollY){
-        document.getElementById("navbar").style.top = "12px";
+        document.getElementById("navbar").style.top = "0px";
     } else {
         document.getElementById("navbar").style.top = "-240px";
 
@@ -8,12 +8,13 @@ document.addEventListener("scroll",(e)=>{
     this.oldScroll = this.scrollY;
 })
 
-// copy_email_btn.addEventListener("click", (e)=>{
-//     e.preventDefault();
+const copy_email_btn = document.getElementById("copy-email");
+copy_email_btn.addEventListener("click", (e)=>{
+    e.preventDefault();
     
-//     navigator.clipboard.writeText("tharun.kumar.a.2006@gmail.com").then(() => {});
-//     copy_email_btn.innerHTML=`<svg  xmlns="http://www.w3.org/2000/svg"  width="18"  height="18"  viewBox="0 0 24 24"  fill="none"  stroke="#00ff00"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-check"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l5 5l10 -10" /></svg>`
-//     setTimeout(()=>{
-//     copy_email_btn.innerHTML=`<svg  xmlns="http://www.w3.org/2000/svg"  width="18"  height="18"  viewBox="0 0 24 24"  fill="none"  stroke="#aaaaaa"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-copy"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 7m0 2.667a2.667 2.667 0 0 1 2.667 -2.667h8.666a2.667 2.667 0 0 1 2.667 2.667v8.666a2.667 2.667 0 0 1 -2.667 2.667h-8.666a2.667 2.667 0 0 1 -2.667 -2.667z" /><path d="M4.012 16.737a2.005 2.005 0 0 1 -1.012 -1.737v-10c0 -1.1 .9 -2 2 -2h10c.75 0 1.158 .385 1.5 1" /></svg>`
-//     }, 2000);
-// })
+    navigator.clipboard.writeText("tharun.kumar.a.2006@gmail.com").then(() => {});
+    copy_email_btn.innerHTML=`<svg  xmlns="http://www.w3.org/2000/svg"  width="18"  height="18"  viewBox="0 0 24 24"  fill="none"  stroke="var(--primary)"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-check"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l5 5l10 -10" /></svg>`
+    setTimeout(()=>{
+    copy_email_btn.innerHTML=`<svg  xmlns="http://www.w3.org/2000/svg"  width="18"  height="18"  viewBox="0 0 24 24"  fill="none"  stroke="var(--text)"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-copy"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 7m0 2.667a2.667 2.667 0 0 1 2.667 -2.667h8.666a2.667 2.667 0 0 1 2.667 2.667v8.666a2.667 2.667 0 0 1 -2.667 2.667h-8.666a2.667 2.667 0 0 1 -2.667 -2.667z" /><path d="M4.012 16.737a2.005 2.005 0 0 1 -1.012 -1.737v-10c0 -1.1 .9 -2 2 -2h10c.75 0 1.158 .385 1.5 1" /></svg>`
+    }, 2000);
+})
