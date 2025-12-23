2f68a0f4c33614ab28cb9c404231a2b60d1271a1
Tharun-Kumar-A-01 <tharunkumar8906@gmail.com>
2025-12-23 12:53:34 +0530

    Changed primary color, ttf to woff2, ui changes

--- script.js
+++ script.js
@@ -18,3 +18,29 @@ copy_email_btn.addEventListener("click", (e)=>{
     copy_email_btn.innerHTML=`<svg  xmlns="http://www.w3.org/2000/svg"  width="18"  height="18"  viewBox="0 0 24 24"  fill="none"  stroke="var(--text)"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-copy"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 7m0 2.667a2.667 2.667 0 0 1 2.667 -2.667h8.666a2.667 2.667 0 0 1 2.667 2.667v8.666a2.667 2.667 0 0 1 -2.667 2.667h-8.666a2.667 2.667 0 0 1 -2.667 -2.667z" /><path d="M4.012 16.737a2.005 2.005 0 0 1 -1.012 -1.737v-10c0 -1.1 .9 -2 2 -2h10c.75 0 1.158 .385 1.5 1" /></svg>`
     }, 2000);
 })
+
+const colors = [
+  "#e74c3c",
+  "#8e44ad",
+  "#3498db",
+  "#16a085",
+  "#f39c12",
+  "#d35400"
+];
+
+const elements = document.querySelectorAll("p.s3tech");
+console.log(elements);
+// copy array so original is untouched
+const shuffledColors = [...colors];
+
+// Fisher–Yates shuffle
+for (let i = shuffledColors.length - 1; i > 0; i--) {
+  const j = Math.floor(Math.random() * (i + 1));
+  [shuffledColors[i], shuffledColors[j]] =
+    [shuffledColors[j], shuffledColors[i]];
+}
+
+// apply unique colors
+elements.forEach((el, i) => {
+    el.style.color = shuffledColors[i % shuffledColors.length];
+});
